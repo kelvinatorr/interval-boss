d6964abcfbe7ef910fdbbaa46bee1ac54f87458e
Kelvin Dealca <kelvin.dealca@gmail.com>
2022-06-09 16:30:05 -0700

    Fix bug where timers can't be set to zero

--- src/Timer.gd
+++ src/Timer.gd
@@ -1,6 +1,8 @@
 extends Control
 class_name LabelTimer
 
+var wait_time: float = 0.0
+
 onready var _my_timer: Timer = $Timer
 onready var _audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 onready var _label: Label = $Label
@@ -11,6 +13,8 @@ onready var _sec_line_edit: LineEdit = $HBoxContainer/SecEdit
 signal timer_timeout(timer)
 
 func _ready() -> void:
+	if wait_time > 0.0:
+		_my_timer.wait_time = wait_time
 	self.show_wait_time()
 
 func _physics_process(delta: float) -> void:
@@ -18,6 +22,9 @@ func _physics_process(delta: float) -> void:
 		self._label.text = Helper.format_time(_my_timer.time_left)
 
 func start() -> void:
+	if wait_time == 0.0:
+		self.emit_signal("timer_timeout", self)
+		return
 	if _my_timer.paused:
 		_my_timer.paused = false
 	else:
@@ -41,11 +48,11 @@ func is_stopped() -> bool:
 	return _my_timer.is_stopped()
 
 func show_wait_time() -> void:
-	self._label.text = Helper.format_time(_my_timer.wait_time)
+	self._label.text = Helper.format_time(wait_time)
 
 func edit() -> void:
 	# Edit the wait time not the current time, because when it saves, the round is reset
-	var time_array: Array = Helper.get_mins_secs(_my_timer.wait_time)
+	var time_array: Array = Helper.get_mins_secs(wait_time)
 	_min_line_edit.set_text(Helper.pad_int(str(time_array[0])))
 	_sec_line_edit.set_text(Helper.pad_int(str(time_array[1])))
 	# Hide the label
@@ -59,7 +66,9 @@ func grab_focus() -> void:
 func save_edit() -> void:
 	# When the edit is saved, the round is reset
 	var total_secs = Helper.calc_total_seconds(_min_line_edit.get_text(), _sec_line_edit.get_text())
-	_my_timer.wait_time = total_secs
+	wait_time = total_secs
+	if total_secs > 0:
+		_my_timer.wait_time = total_secs
 	if not is_stopped():
 		_my_timer.paused = false
 		_my_timer.stop()
